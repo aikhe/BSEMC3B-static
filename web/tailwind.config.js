77b17d0881239a90964c6990d817bc63bfe5372b
Luoie8340 <itslukiee@gmail.com>
2024-05-28 23:18:20 +0800

    vite mounted React + Typescript (alongside tailwind for styling)

---
+++ web/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx}",
+    "./layouts/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
